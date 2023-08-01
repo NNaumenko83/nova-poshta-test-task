8659051f10eb1a61b74662011c74bfed18602937
Nikolay Naumenko <nnv2006@ukr.net>
2023-08-01 14:58:51 +0300

    ADD: routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { SharedLayout } from "./components/SharedLayout/SharedLayout";
+import { Home } from "./pages/Home/Home";
+import { Warehouses } from "./pages/Warehouses/Warehouses";
 
 function App() {
 	return (
 		<>
-			<h1>NOVA POSHTA</h1>
+			<Routes>
+				<Route path="/" element={<SharedLayout />}>
+					<Route index element={<Home />} />
+					<Route path="warehouses" element={<Warehouses />} />
+				</Route>
+			</Routes>
 		</>
 	);
 }
