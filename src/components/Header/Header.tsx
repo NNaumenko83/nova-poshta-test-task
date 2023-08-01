08b65e5438832e4f0cb9a3ddff5a8881ebbcad8e
Nikolay Naumenko <nnv2006@ukr.net>
2023-08-01 15:57:18 +0300

    CHANGE: NavLink names

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { Container } from "../Container/Container";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Header: React.FC = () => {
 	return (
@@ -7,8 +7,8 @@ export const Header: React.FC = () => {
 			<Container>
 				<h1>NovaPoshta</h1>
 				<nav>
-					<Link to="/">Home</Link>
-					<Link to="/warehouses">About</Link>
+					<NavLink to="/">Перевірити ТТН</NavLink>
+					<NavLink to="/warehouses">Список відділень</NavLink>
 				</nav>
 				{/* <img src={logo} alt="Nova Poshta Logo" style={{ height: "40px" }} /> */}
 			</Container>
