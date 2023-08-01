073880e8827481730e40ed744124ed5898c2a196
Nikolay Naumenko <nnv2006@ukr.net>
2023-08-01 16:01:46 +0300

    ADD: Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "../Container/Container";
+
+export const Footer: React.FC = () => {
+	return (
+		<footer>
+			<Container>
+				<p>Created by Mykola Naumenko</p>
+			</Container>
+		</footer>
+	);
+};
