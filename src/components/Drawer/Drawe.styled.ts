ea91e988d19a5d77ab1ee0ad2e33a34ac11cb4b0
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-08-02 00:14:50 +0300

    ADD styles for links

--- src/components/Drawer/Drawe.styled.ts
+++ src/components/Drawer/Drawe.styled.ts
@@ -2,4 +2,5 @@ import { styled } from "styled-components";
 
 export const MobileNavigationWrapper = styled.div`
 	height: 100%;
+	background-color: #df012e;
 `;
--- src/components/NavigateMenu/NavigateMenu.tsx
+++ src/components/NavigateMenu/NavigateMenu.tsx
@@ -8,9 +8,25 @@ const NavLinkStyled = styled(NavLink)`
 	align-items: center;
 	padding: 5px;
 	width: 200px;
-	border: 1px solid red;
-	border-radius: 10px;
+
+	color: ${props => props.theme.colors.brightTxt};
 	font-size: ${props => props.theme.fontSizes.xsmall};
+	&.active {
+		color: white;
+		border: 1px solid ${props => props.theme.colors.brightTxt};
+		background-color: #ff0044;
+
+		border-radius: 10px;
+		-moz-box-shadow: 1px 0px 15px 0px rgba(245, 245, 245, 1);
+		box-shadow: 1px 0px 15px 0px rgba(245, 245, 245, 1);
+	}
+
+	transition: color, text-shadow 300ms linear;
+	&:hover,
+	&:focus {
+		color: #ffd42f;
+		text-shadow: 0px 0px 3px #ceb30a;
+	}
 `;
 
 export const NavigateMenu: React.FC = () => {
