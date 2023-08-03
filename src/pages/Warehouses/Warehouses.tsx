a999864203faa5709576c2cf1bbfb12fc4c1758b
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-08-03 23:36:27 +0300

    ADD: debaunced for request

--- src/pages/Warehouses/Warehouses.tsx
+++ src/pages/Warehouses/Warehouses.tsx
@@ -1,3 +1,31 @@
+import { useState, useEffect } from "react";
+import { useDebouncedCallback } from "use-debounce";
+
+import FocusOutlineInput from "../../components/Input/Input";
+import { Main } from "../../components/Main/Main";
+
 export const Warehouses = () => {
-	return <h1>Warehouses</h1>;
+	const [value, setValue] = useState("");
+
+	const onChange: React.ChangeEventHandler<HTMLInputElement> = e => {
+		setValue(e.target.value);
+	};
+
+	const getWarehauses = () => {
+		console.log("getWarehauses");
+	};
+
+	const debouncedSendRequest = useDebouncedCallback(getWarehauses, 1000);
+
+	useEffect(() => {
+		debouncedSendRequest();
+		return () => debouncedSendRequest.cancel();
+	}, [value, debouncedSendRequest]);
+
+	return (
+		<Main>
+			<h1>Warehouses</h1>
+			<FocusOutlineInput value={value} onChange={onChange} placeholder="Введіть місто" />
+		</Main>
+	);
 };
