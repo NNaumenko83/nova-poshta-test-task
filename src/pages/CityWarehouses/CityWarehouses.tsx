af2224a8f206123ddd337b69ed2ffb282e866bf9
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-08-05 17:08:35 +0300

    FIX: onChangeType

--- src/pages/CityWarehouses/CityWarehouses.tsx
+++ src/pages/CityWarehouses/CityWarehouses.tsx
@@ -5,27 +5,31 @@ import { UnstyledSelectControlled } from "../../components/Select/Select";
 import { useEffect, useState } from "react";
 import { TWarehouse } from "../../Types/WarehouseType";
 import { getWarehousesTypes } from "../../services/api";
+import { getErrorMessage } from "../../utils/getErrorMessage";
 
 export const СityWarehouses = () => {
 	const [warehousesTypes, setWarehousesTypes] = useState<TWarehouse[]>([]);
 	const [searchParams, setSearchParams] = useSearchParams();
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState("");
 
 	const type = searchParams.get("type");
-	console.log("type:", type);
-
-	// const [isLoading, setIsLoading] = useState(false);
-	// const [error, setError] = useState("");
 
 	const { city } = useParams();
 	console.log("city:", city);
 
-	const onChangeType = (type: string) => {
-		setSearchParams({ type });
+	const onChangeType = (type: string | null) => {
+		const params = new URLSearchParams();
+		if (type !== null) {
+			params.append("type", type);
+		}
+		setSearchParams(params);
 	};
 
 	useEffect(() => {
 		(async () => {
-			// setIsLoading(true);
+			setIsLoading(true);
+			console.log("useEffect");
 			try {
 				const data = await getWarehousesTypes();
 				setWarehousesTypes(data);
@@ -36,13 +40,12 @@ export const СityWarehouses = () => {
 				}
 			} catch (error) {
 				console.log("error:", error);
-				// setError(getErrorMessage(error));
+				setError(getErrorMessage(error));
 			} finally {
-				// setIsLoading(false);
+				setIsLoading(false);
 			}
 		})();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [setSearchParams, type]);
 
 	return (
 		<Main>
@@ -50,8 +53,8 @@ export const СityWarehouses = () => {
 			{warehousesTypes.length > 0 && (
 				<UnstyledSelectControlled warehousesTypes={warehousesTypes} onChangeType={onChangeType} value={type} />
 			)}
-			{/* {isLoading && <h2>LOADING....</h2>}
-			{error && <h2>{error}</h2>} */}
+			{isLoading && <h2>LOADING....</h2>}
+			{error && <h2>{error}</h2>}
 		</Main>
 	);
 };
